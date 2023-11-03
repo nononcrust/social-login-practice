77ca5673e64943976484cfbcafaf85c782234b97
nonon <nononcrust@gmail.com>
2023-11-03 21:55:12 +0900

    버그 수정 및 env example 추가

--- src/features/kakao.ts
+++ src/features/kakao.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { userInfoSchema } from "./auth";
 
 const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!;
-const KAKAO_CLIENT_SECRET = process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!;
+const KAKAO_CLIENT_SECRET = process.env.KAKAO_CLIENT_SECRET!;
 const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI!;
 export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
